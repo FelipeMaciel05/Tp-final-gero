7d1276413db45be04dbbb0d9300d763bf8b98de9
FelipeMaciel05 <felimorita05@gmail.com>
2022-06-01 14:13:29 -0300

    uwu

--- Assets/Movimiento.cs
+++ Assets/Movimiento.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class Movimiento : MonoBehaviour
 {
-    public float speed;
-    public float rotationspeed;
-
     void Start()
     {
         
@@ -16,5 +13,13 @@ public class Movimiento : MonoBehaviour
     void Update()
     {
         transform.Translate(-0.1f, 0, 0);
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(0, 0, 0.3f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -0.3f);
+        }
     }
 }
