f703936f0598ef5e18e8dcb452b151dd70dcd4c9
FelipeMaciel05 <felimorita05@gmail.com>
2022-06-15 15:55:11 -0300

    kaleb

--- Assets/SceneManagerScript.cs
+++ Assets/SceneManagerScript.cs
@@ -29,7 +29,7 @@ public class SceneManagerScript : MonoBehaviour
         }
         if (col.gameObject.tag == "Perdes")
         {
-            SceneManager.LoadScene("Escena 2");
+            SceneManager.LoadScene("Escena 3");
             transform.position = new Vector3(99, 0.5f, 0);
         }
     }
@@ -46,5 +46,13 @@ public class SceneManagerScript : MonoBehaviour
     public void Menu()
     {
         SceneManager.LoadScene("Inicio");
+    }
+    public void Nivel2()
+    {
+        SceneManager.LoadScene("Nivel 2");
+    }
+    public void MenuNiveles()
+    {
+        SceneManager.LoadScene("MenuNiveles");
     }
 }
